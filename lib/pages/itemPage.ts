b1a989b93e2d9d21f8dd4ca79c5ddb6b5bbd3fee
Pierre Mondejar <pierremondejar@gmail.com>
2026-06-29 22:18:45 +0800

    Feature: Added handling on verifying product images on image pages

--- lib/pages/itemPage.ts
+++ lib/pages/itemPage.ts
@@ -8,6 +8,7 @@ export class ItemPage extends InventoryPage {
     readonly itemName: Locator;
     readonly itemDescription: Locator;
     readonly itemPrice: Locator;
+    readonly itemImage: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -15,7 +16,7 @@ export class ItemPage extends InventoryPage {
         this.itemName = page.getByTestId('inventory-item-name');
         this.itemDescription = page.getByTestId('inventory-item-desc');
         this.itemPrice = page.getByTestId('inventory-item-price');
-        
+        this.itemImage = page.locator('.inventory_details_img')
     }
 
     async selectItem(index: number) {
@@ -33,8 +34,12 @@ export class ItemPage extends InventoryPage {
     }
 
     async verifyItemDetails(product:Product){
+        //const test =  this.itemImageContainer.filter({has: this.page.locator('.inventory_details_img')})
         await expect(this.itemName).toHaveText(product.productName);
         await expect(this.itemDescription).toHaveText(product.productDescription);
         await expect(this.itemPrice).toHaveText(`$${product.productPrice}`);
+        await expect(this.itemImage).toHaveScreenshot(product.productName+'.png', {maxDiffPixelRatio: 0.01});
     }
+
+
 }
