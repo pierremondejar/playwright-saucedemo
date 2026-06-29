30e85613c517c7a68f9132ad8f61203e636efd85
Pierre Mondejar <pierremondejar@gmail.com>
2026-06-29 16:05:38 +0800

    Fix: Fixed the ENV variables for Auth Setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,10 +5,6 @@ setup('Authenticate', async ({ page}) => {
     const password = process.env.STANDARD_PASSWORD!;
     const baseURL = process.env.BASEURL!;
 
-    if (!baseURL) throw new Error('Environment variable BASEURL is not set');
-    if (!username) throw new Error('Environment variable STANDARD_USERNAME is not set');
-    if (!password) throw new Error('Environment variable STANDARD_PASSWORD is not set');
-
     await page.goto(baseURL);
     await page.getByRole('textbox', {name: 'Username'}).fill(username);
     await page.getByRole('textbox', {name: 'Password'}).fill(password);
