4ff83d602313a5c42032f0dea3c744c6d0e5496b
Pierre Mondejar <pierremondejar@gmail.com>
2026-06-29 16:10:37 +0800

    Fix: Fixed the ENV variables for Auth Setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASEURL,
+    baseURL: process.env.BASEURL!,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-test',
