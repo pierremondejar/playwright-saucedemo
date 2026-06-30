c34298cfe7b2d99460942b810de692621c65d49c
Pierre Mondejar <pierremondejar@gmail.com>
2026-06-30 17:22:16 +0800

    Feature: Added handling in verifying Adding and Removing Cart Items via Item Page

--- lib/pages/itemPage.ts
+++ lib/pages/itemPage.ts
@@ -9,6 +9,8 @@ export class ItemPage extends InventoryPage {
     readonly itemDescription: Locator;
     readonly itemPrice: Locator;
     readonly itemImage: Locator;
+    readonly addToCartButton: Locator;
+    readonly removeFromCartButton: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -16,7 +18,9 @@ export class ItemPage extends InventoryPage {
         this.itemName = page.getByTestId('inventory-item-name');
         this.itemDescription = page.getByTestId('inventory-item-desc');
         this.itemPrice = page.getByTestId('inventory-item-price');
-        this.itemImage = page.locator('.inventory_details_img')
+        this.itemImage = page.locator('.inventory_details_img');
+        this.addToCartButton = page.getByRole('button', {name: 'Add to cart'});
+        this.removeFromCartButton = page.getByRole('button', {name: 'Remove'});
     }
 
     async selectItem(index: number) {
@@ -38,6 +42,29 @@ export class ItemPage extends InventoryPage {
         await expect(this.itemDescription).toHaveText(product.productDescription);
         await expect(this.itemPrice).toHaveText(`$${product.productPrice}`);
         await expect(this.itemImage).toHaveScreenshot(product.productName+'.png', {maxDiffPixelRatio: 0.01});
+    }
+
+    async addItemsToCart(products: Product[]) {
+        for (let index = 0; index < products.length; index++) {
+            await this.selectItem(index);
+            await this.addToCartButton.click();
+            await this.page.goBack();
+            await expect(this.cartBadge).toHaveText(String(index+1)) 
+        }
+    }
+
+    async removeItemsToCart(products: Product[]) {
+        let count = products.length;
+        for (let index = 0; index < count; index++) {
+            await this.selectItem(index);
+            await this.removeFromCartButton.click();
+            await this.page.goBack();
+            if(count-(index+1) != 0) {
+                await expect(this.cartBadge).toHaveText(String(count-(index+1))) }
+            else {
+                await expect(this.cartBadge).toBeHidden();
+            }
+        }
     }
 
 
--- tests/itemPage.spec.ts
+++ tests/itemPage.spec.ts
@@ -9,7 +9,14 @@ test.describe('Inventory Page', () => {
     test('should be able to display product information on item pages correctly', async({itemPage}) => {
         await itemPage.verifyAllProductDetails(ProductData.products);
     })
-    
 
+    test('should be able to add products to the cart from the item page', async({itemPage}) => {
+        await itemPage.addItemsToCart(ProductData.products);
+    })
+
+    test('should be able to remove products to the cart from the item page', async({itemPage}) => {
+        await itemPage.addItemsToCart(ProductData.products);
+        await itemPage.removeItemsToCart(ProductData.products);
+    })
 
 })
